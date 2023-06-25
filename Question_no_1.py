d11072fc4553d1c5c564e7c94b5b6992c5de2395
Yogitakanyal15 <121041356+Yogitakanyal15@users.noreply.github.com>
2023-06-25 22:16:04 +0530

    Question_no_1.py

---
+++ Question_no_1.py
@@ -0,0 +1,11 @@
+def move(l):
+  n=[]
+  for i in l:
+    if (l[i]!=0):
+      n.append(l[i])
+  for i in l:
+    if (l[i]==0):
+      n.append(l[i])
+  return n
+arr=[]0,1,0,3,1,2] 
+print(move(l))
